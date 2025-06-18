0b921925e52f8c6ddf6cfbb6ae110d91ddfa76c1
ArpitBansal3006 <arpitbansal717@gmail.com>
2025-06-18 09:47:38 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Route,Routes} from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
 
+// function App() {
+//   return (
+//     <div className="container text-center">
+//       <h1>Welcome to MERN Projects</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <div className="container text-center">
-      <h1>Welcome to MERN Projects</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
