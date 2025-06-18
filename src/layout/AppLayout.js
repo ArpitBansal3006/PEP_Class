665175d745f4adcb8053391b4d3c723dfdd3f2bb
ArpitBansal3006 <arpitbansal717@gmail.com>
2025-06-18 10:39:54 +0530

    Creating different components

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -1,19 +1,16 @@
-import {Link} from "react-router-dom";
+
+import Header from "./Header";
+import Footer from "./Footer";
 
 function AppLayout({children}){
     return(
         <>
-        {/*Header */}
-        <div className="container">
-            <Link to='/'>Home</Link>
-            <Link to="/login">Login</Link>
-        </div>
+      
+       <Header/>
         {children}
-
-        {/* Footer */}
-        <div className="container text-center">
-            Footer Component
-        </div>
+       <Footer/>
+        
+        
         </>
     );
 }
